13f476f0f2c92ad59ff3bde767363625728c567b
Nadia Khan <83128763+Nadiakhanv2@users.noreply.github.com>
2022-07-28 23:20:47 +0500

    about section started

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,10 +2,36 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      this is the about page
-    </div>
-  )
+    <section className="about section">
+      <div className="container-fluid p-5">
+        <div className="row">
+          <div className="section-title px-5">
+            <h2>About Me</h2>
+          </div>
+        </div>
+        <div className="row">
+          <div className="about-content px-4">
+            <div className="row">
+              <div className="about-text px-5">
+                <h3>
+                  I' m Nadya Khan and <span>Web Developer</span>
+                </h3>
+                <p>
+                  I'm a web developer with a passion for building beautiful,
+                  functional websites. I'm currently working as a freelancer,
+                  but I'm always looking for new opportunities to learn and
+                  grow.I'm a web developer with a passion for building
+                  beautiful, functional websites. I'm currently working as a
+                  freelancer, but I'm always looking for new opportunities to
+                  learn and grow.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default About
