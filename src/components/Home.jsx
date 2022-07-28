043722631d97f02a86219653f2a8ef50b484e9ca
Nadia Khan <83128763+Nadiakhanv2@users.noreply.github.com>
2022-07-28 18:50:12 +0500

    home section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      home page
-    </div>
-  )
-}
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="home-info">
+            <h3 className="heloo">Hello , my name is <span className="name">Nadya Khan</span></h3>
+            <h3 className="my-profession">I' m a <span className="typing">Front end web developer</span></h3>
+            <p>
+              I'm a web developer with a passion for building beautiful,
+              functional websites. I'm currently working as a freelancer,
+              but I'm always looking for new opportunities to learn and grow.
 
-export default Home
+            </p>
+            <a href="" className="btn hire-me">Hire Me</a>
+          </div>
+          <div className="home-img">
+            <img src="" alt="" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
