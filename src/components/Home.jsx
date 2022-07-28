daeba740352e10eecd91d9be9a6737e9611689ff
Nadia Khan <83128763+Nadiakhanv2@users.noreply.github.com>
2022-07-28 21:08:46 +0500

    modified

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,23 +8,23 @@ const Home = () => {
       <div className="home section">
       <div className="container-h container-fluid">
         <div className="row">
-          <div className="home-info">
-            <h3 className="heloo">
+          <div className="col-7 home-info p-5 my-5">
+            <h3 className="heloo fs-2" >
               Hello , my name is <span className="name">Nadya Khan</span>
             </h3>
-            <h3 className="my-profession">
+            <h3 className="my-profession fs-3 my-3">
               I' m a <span className="typing">Front end web developer</span>
             </h3>
-            <p>
+            <p className="fs-5 mb-4">
               I'm a web developer with a passion for building beautiful,
               functional websites. I'm currently working as a freelancer, but
               I'm always looking for new opportunities to learn and grow.
             </p>
-            <a href="" className="btn hire-me">
+            <a href="" className="btn-blog hire-me">
               Hire Me
             </a>
           </div>
-          <div className="home-img">
+          <div className="col-5 home-img text-center p-5">
             <img
               src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80"
               alt="img" 
