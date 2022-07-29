670a38656fe231a3a4170e42bf12760c7b78712c
Nadia Khan <83128763+Nadiakhanv2@users.noreply.github.com>
2022-07-29 22:07:30 +0500

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import Home from './components/Home';
+import About from './components/About';
+// import Home from './components/Home';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-    <Home/>
+      {/* <Home/> */}
+      <About/>
     </div>
   );
 }
