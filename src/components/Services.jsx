2000b1c5e5f6d594a22e9fde2cdb73af4b3d0b70
Nadia Khan <83128763+Nadiakhanv2@users.noreply.github.com>
2022-08-06 16:08:17 +0500

    page done

---
+++ src/components/Services.jsx
@@ -0,0 +1,166 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <section className="service section-p">
+      <div className="container">
+        <div className="row">
+          <div className="section-title px-5">
+            <h2>Services</h2>
+          </div>
+        </div>
+        <div className="row">
+          {/* === service item start */}
+          <div className="service-item px-5">
+            <div className="service-item-inner">
+              <div className="icon">
+                <i class="bi bi-phone"></i>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="40"
+                  height="40"
+                  fill="currentColor"
+                  class="bi bi-phone"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z" />
+                  <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+                </svg>
+              </div>
+              <h4>Web Design</h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quisquam, quidem.
+              </p>
+            </div>
+          </div>
+          {/* service item end */}
+          {/* === service item start */}
+          <div className="service-item px-5">
+            <div className="service-item-inner">
+              <div className="icon">
+                <i class="bi bi-laptop"></i>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="40"
+                  height="40"
+                  fill="currentColor"
+                  class="bi bi-laptop"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M13.5 3a.5.5 0 0 1 .5.5V11H2V3.5a.5.5 0 0 1 .5-.5h11zm-11-1A1.5 1.5 0 0 0 1 3.5V12h14V3.5A1.5 1.5 0 0 0 13.5 2h-11zM0 12.5h16a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 12.5z" />
+                </svg>
+              </div>
+              <h4>Web Design</h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quisquam, quidem.
+              </p>
+            </div>
+          </div>
+          {/* service item end */}
+          {/* === service item start */}
+          <div className="service-item px-5">
+            <div className="service-item-inner">
+              <div className="icon">
+                <i class="bi bi-palette-fill"></i>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="40"
+                  height="40"
+                  fill="currentColor"
+                  class="bi bi-palette-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M12.433 10.07C14.133 10.585 16 11.15 16 8a8 8 0 1 0-8 8c1.996 0 1.826-1.504 1.649-3.08-.124-1.101-.252-2.237.351-2.92.465-.527 1.42-.237 2.433.07zM8 5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm4.5 3a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zM5 6.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm.5 6.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
+                </svg>
+              </div>
+              <h4>Web Design</h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quisquam, quidem.
+              </p>
+            </div>
+          </div>
+          {/* service item end */}
+          {/* === service item start */}
+          <div className="service-item px-5">
+            <div className="service-item-inner">
+              <div className="icon">
+                <i class="bi bi-code-slash"></i>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="40"
+                  height="40"
+                  fill="currentColor"
+                  class="bi bi-code-slash"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M10.478 1.647a.5.5 0 1 0-.956-.294l-4 13a.5.5 0 0 0 .956.294l4-13zM4.854 4.146a.5.5 0 0 1 0 .708L1.707 8l3.147 3.146a.5.5 0 0 1-.708.708l-3.5-3.5a.5.5 0 0 1 0-.708l3.5-3.5a.5.5 0 0 1 .708 0zm6.292 0a.5.5 0 0 0 0 .708L14.293 8l-3.147 3.146a.5.5 0 0 0 .708.708l3.5-3.5a.5.5 0 0 0 0-.708l-3.5-3.5a.5.5 0 0 0-.708 0z" />
+                </svg>
+              </div>
+              <h4>Web Design</h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quisquam, quidem.
+              </p>
+            </div>
+          </div>
+          {/* service item end */}
+
+          {/* service item end */}
+          {/* === service item start */}
+          <div className="service-item px-5">
+            <div className="service-item-inner">
+              <div className="icon">
+                <i class="bi bi-search"></i>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="40"
+                  height="40"
+                  fill="currentColor"
+                  class="bi bi-search"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                </svg>
+              </div>
+              <h4>Web Design</h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quisquam, quidem.
+              </p>
+            </div>
+          </div>
+          {/* service item end */}
+          {/* === service item start */}
+          <div className="service-item px-5">
+            <div className="service-item-inner">
+              <div className="icon">
+                <i class="bi bi-megaphone"></i>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="40"
+                  height="40"
+                  fill="currentColor"
+                  class="bi bi-megaphone"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M13 2.5a1.5 1.5 0 0 1 3 0v11a1.5 1.5 0 0 1-3 0v-.214c-2.162-1.241-4.49-1.843-6.912-2.083l.405 2.712A1 1 0 0 1 5.51 15.1h-.548a1 1 0 0 1-.916-.599l-1.85-3.49a68.14 68.14 0 0 0-.202-.003A2.014 2.014 0 0 1 0 9V7a2.02 2.02 0 0 1 1.992-2.013 74.663 74.663 0 0 0 2.483-.075c3.043-.154 6.148-.849 8.525-2.199V2.5zm1 0v11a.5.5 0 0 0 1 0v-11a.5.5 0 0 0-1 0zm-1 1.35c-2.344 1.205-5.209 1.842-8 2.033v4.233c.18.01.359.022.537.036 2.568.189 5.093.744 7.463 1.993V3.85zm-9 6.215v-4.13a95.09 95.09 0 0 1-1.992.052A1.02 1.02 0 0 0 1 7v2c0 .55.448 1.002 1.006 1.009A60.49 60.49 0 0 1 4 10.065zm-.657.975 1.609 3.037.01.024h.548l-.002-.014-.443-2.966a68.019 68.019 0 0 0-1.722-.082z" />
+                </svg>
+              </div>
+              <h4>Web Design</h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quisquam, quidem.
+              </p>
+            </div>
+          </div>
+          {/* service item end */}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Services
