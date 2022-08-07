fe577cf6bd099a9463f7edeb097a9da23b368a5c
Nadia Khan <83128763+Nadiakhanv2@users.noreply.github.com>
2022-08-07 23:02:51 +0500

    todo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,73 @@
 import React from "react";
+import {
+  
+  BsSearch,
+} from "react-icons/bs";
 // import logo from "./public/logo.png"
 
 function Navbar() {
   return (
-    <div>
-      <nav className="navbar bg-light">
-        <div className="container">
-          <a className="navbar-brand" href="/">
-            About
-            {/* <img
-              src={logo}
-              alt=""
-              width="60"
-              height="24"
-            /> */}
+    <header className="header-primary">
+      <nav className="navbar navbar-light navbar-expand-lg ">
+        <div className="container-fluid flex">
+          <a href="/" className="plx-navbar navbar-brand text-blog">
+            <img src="" alt="" width={63} className="img-fluid" />
           </a>
+          <div className="plx-navbar collapse navbar-collapse">
+            <ul className="navbar-nav me-auto pt-0 pb-0">
+              <li className="nav-item fs-2 ">
+                <a
+                  href="/"
+                  className="nav-link nav-link-primary active-primary"
+                >
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="/about" className="nav-link nav-link-primary">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="/Services" className="nav-link nav-link-primary ">
+                  Services
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a href="/Portfolio" className="nav-link nav-link-primary ">
+                  Portfolio
+                </a>
+              </li>
+            </ul>
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a
+                  href="/"
+                  className="nav-link nav-link-primary"
+                  aria-controls="search"
+                  aria-expanded="false"
+                >
+                  <i className="blog-clr" aria-hidden="true">
+                    <BsSearch className="fa" />
+                  </i>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="/Contact" className="nav-link nav-link-primary ">
+                  Contact
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="/Services" className="nav-link nav-link-primary ">
+                  Services
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
-    </div>
+    </header>
   );
 }
 
