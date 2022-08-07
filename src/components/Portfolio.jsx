cf4dfd9827e9bfa6d98fd3779f4e0c3c3c90e358
Nadia Khan <83128763+Nadiakhanv2@users.noreply.github.com>
2022-08-07 23:03:38 +0500

    todo

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,11 +1,90 @@
-import React from 'react'
+import React from "react";
 
 const Portfolio = () => {
   return (
-    <div>
-      portfolio
-    </div>
+    <section className="portfolio section">
+      <div className="container mt-4">
+        <div className="row">
+          <div className="section-title">
+            <h2>Portfolio</h2>
+          </div>
+        </div>
+        <div className="row">
+          <div className="portfolio-heading">
+            <h2>My Last Projects:</h2>
+          </div>
+        </div>
+        <div className="row">
+          {/* ============= portfolio item ======= */}
+          <div className="portfolio-item">
+            <div className="portfolio-item-inner shadow-dark">
+              <div className="portfolio-img">
+                <img
+                  src="https://www.qualdev.com/wp-content/uploads/2022/07/Website-Development-Pro-Cons.png"
+                  alt="img"
+                />
+              </div>
+            </div>
+          </div>
+          {/* ============= portfolio item ======= */}
+          <div className="portfolio-item">
+            <div className="portfolio-item-inner shadow-dark">
+              <div className="portfolio-img">
+                <img
+                  src="https://www.qualdev.com/wp-content/uploads/2022/07/Website-Development-Pro-Cons.png"
+                  alt="img"
+                />
+              </div>
+            </div>
+          </div>
+          {/* ============= portfolio item ======= */}
+          <div className="portfolio-item">
+            <div className="portfolio-item-inner shadow-dark">
+              <div className="portfolio-img">
+                <img
+                  src="https://www.qualdev.com/wp-content/uploads/2022/07/Website-Development-Pro-Cons.png"
+                  alt="img"
+                />
+              </div>
+            </div>
+          </div>
+          {/* ============= portfolio item ======= */}
+          <div className="portfolio-item">
+            <div className="portfolio-item-inner shadow-dark">
+              <div className="portfolio-img">
+                <img
+                  src="https://www.qualdev.com/wp-content/uploads/2022/07/Website-Development-Pro-Cons.png"
+                  alt="img"
+                />
+              </div>
+            </div>
+          </div>
+          {/* ============= portfolio item ======= */}
+          <div className="portfolio-item">
+            <div className="portfolio-item-inner shadow-dark">
+              <div className="portfolio-img">
+                <img
+                  src="https://www.qualdev.com/wp-content/uploads/2022/07/Website-Development-Pro-Cons.png"
+                  alt="img"
+                />
+              </div>
+            </div>
+          </div>
+          {/* ============= portfolio item ======= */}
+          <div className="portfolio-item">
+            <div className="portfolio-item-inner shadow-dark">
+              <div className="portfolio-img">
+                <img
+                  src="https://www.qualdev.com/wp-content/uploads/2022/07/Website-Development-Pro-Cons.png"
+                  alt="img"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
-}
+};
 
-export default Portfolio
+export default Portfolio;
