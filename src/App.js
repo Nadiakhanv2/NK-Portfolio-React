6f1f844f75f648a7d6cf1d2493e03ba57ae7aa5c
Nadia Khan <83128763+Nadiakhanv2@users.noreply.github.com>
2022-08-11 18:58:19 +0500

    .

--- src/App.js
+++ src/App.js
@@ -7,34 +7,38 @@ import Navbar from './components/Navbar';
 import Services from './components/Services';
 import Portfolio from './components/Portfolio';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import Pallets from './components/Pallets';
 
 
 function App() {
   return (
     <>
-    <div className="App">
-      <Router>
-          <Navbar logo={ logo} />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/about">
-            <About />
-          </Route>
-          <Route exact path="/portfolio">
-            <Portfolio />
-          </Route>
-          <Route exact path="/services">
-            <Services />
-          </Route>
-          <Route exact path="/contact">
-            <Contact />
-          </Route>
-        </Switch>
-      </Router>
+      <div className="App">
+        <Router>
+          <Navbar logo={logo} />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/about">
+              <About />
+            </Route>
+            <Route exact path="/portfolio">
+              <Portfolio />
+            </Route>
+            <Route exact path="/services">
+              <Services />
+            </Route>
+            <Route exact path="/contact">
+              <Contact />
+            </Route>
+            {/* <Route exact path="/pallets">
+              <Pallets />
+            </Route> */}
+          </Switch>
+        </Router>
       </div>
-      </>
+    </>
   );
 }
 
